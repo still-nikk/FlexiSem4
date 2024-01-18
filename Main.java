7f2a4809570da6f9421c1864781a505efb5f206f
still-nikk <nmaheshwari2904@gmail.com>
2024-01-18 17:33:40 +0530

    Added via intelliJ

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String name1 = "Aman and Akku";
+        System.out.println(name1.substring(5,name1.length()));
+    }
+}
